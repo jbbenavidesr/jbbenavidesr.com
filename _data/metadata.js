98c9efc156aeb34674952d6d8aa355614a9e10e7
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2023-06-27 19:21:10 +0200

    :tada: Restart blog again

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Juan Ber',
+    url: 'https://jbbenavidesr.com/',
+    language: 'es',
+    description: 'Blog de Juan Ber: Viajes, Tecnologia, Física, Religión, etc.',
+    author: {
+        name: 'Juan Bernardo Benavides Rubio',
+        email: 'jbbenavidesr@gmail.com',
+        url: 'https://jbbenavidesr.com/about-me/',
+    },
+};
--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Juan Bernardo Benavides Rubio';
-export const SITE_DESCRIPTION = 'Welcome to my digital space.';
-export const NAME = 'Juan Bernardo Benavides Rubio';
